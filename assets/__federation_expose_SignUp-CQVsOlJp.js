f928804f60774006ecc1c8ee397b3469c7e54c32
JORGE GUILLEN <jguillendev@gmail.com>
2024-04-14 16:27:58 -0600

    starting

---
+++ assets/__federation_expose_SignUp-CQVsOlJp.js
@@ -0,0 +1,7 @@
+import { j as jsxRuntimeExports } from './jsx-runtime-CJjjWGOX.js';
+
+const SignUp = () => {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx("div", { children: /* @__PURE__ */ jsxRuntimeExports.jsx("h1", { children: "Registrarse View" }) });
+};
+
+export { SignUp };
