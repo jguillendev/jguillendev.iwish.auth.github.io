be24ad8eae898002f550a9e65bd993afd11fc47c
Jogini2020 <124850601+Jogini2020@users.noreply.github.com>
2024-04-15 08:58:38 +0000

    Deploying to gh-pages from @ jguillendev/jguillendev.iwish.auth.github.io@ba53738106b89dbb7068f646954098f331bb6bbc 🚀

---
+++ assets/__federation_expose_App-DU_muKat.js
@@ -0,0 +1,7 @@
+import { j as jsxRuntimeExports } from './jsx-runtime-CJjjWGOX.js';
+
+function App() {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx("section", { children: /* @__PURE__ */ jsxRuntimeExports.jsx("h1", { children: "Welcome to  IWish - Auth Federated 4 Pages" }) });
+}
+
+export { App as default };
---
+++ assets/index-BWxoM6fR.js
@@ -0,0 +1,17 @@
+import { importShared } from './__federation_fn_import-BcdbRT-4.js';
+import { j as jsxRuntimeExports } from './jsx-runtime-CJjjWGOX.js';
+import { r as reactDomExports } from './__federation_shared_react-dom-6cCw-6Kw.js';
+import App from './__federation_expose_App-DU_muKat.js';
+
+var client = {};
+
+var m = reactDomExports;
+{
+  client.createRoot = m.createRoot;
+  client.hydrateRoot = m.hydrateRoot;
+}
+
+const React = await importShared('react');
+client.createRoot(document.getElementById("root")).render(
+  /* @__PURE__ */ jsxRuntimeExports.jsx(React.StrictMode, { children: /* @__PURE__ */ jsxRuntimeExports.jsx(App, {}) })
+);
