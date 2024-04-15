ba53738106b89dbb7068f646954098f331bb6bbc
JORGE GUILLEN <jguillendev@gmail.com>
2024-04-15 02:58:10 -0600

    updates 45

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <section>
-      <h1>Welcome to  IWish - Auth Federated 3 Pages</h1>
+      <h1>Welcome to  IWish - Auth Federated 4 Pages</h1>
     </section>
   )
 }
